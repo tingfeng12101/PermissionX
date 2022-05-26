b9401349b68567afeeb17e7f97d79f3963e08682
tingfeng12101 <tingyuxuan2012@126.com>
2022-05-26 22:12:39 +0800

    在library中编写申请权限的功能

--- app/src/main/java/cn/yxm/permissionx/MainActivity.kt
+++ app/src/main/java/cn/yxm/permissionx/MainActivity.kt
@@ -1,11 +1,40 @@
 package cn.yxm.permissionx
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import cn.permission.yxm.PermissionX
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.btn_call).setOnClickListener {
+
+            PermissionX.request(this, Manifest.permission.CALL_PHONE) { allGranted, _ ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "请开启相关权限", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 }
